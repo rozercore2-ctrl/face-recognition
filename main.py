bd246558b8100fd7471b5a40a5184978020a39bf
rozercore2-ctrl <rozercore2@gmail.com>
2026-01-28 14:48:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from register import register_face  # fungsi registrasi
-from train import train_model, load_label_dict  # fungsi training
-from recognize_with_gsheets import recognize_face  # recognition yg sudah include upload sheets
-
+from register import register_face 
+from train import train_model, load_label_dict  
+from recognize_with_gsheets import recognize_face  
 def main():
     label_dict = None
     while True:
@@ -12,9 +11,7 @@ def main():
         choice = input("Pilih menu: ").strip()
 
         if choice == '1':
-            # Jalankan registrasi
             register_face()
-            # Setelah registrasi selesai, jalankan training otomatis
             print("Registrasi selesai. Menjalankan training otomatis...")
             label_dict = train_model()
             if label_dict:
@@ -22,13 +19,12 @@ def main():
             else:
                 print("Training gagal. Periksa dataset.")
         elif choice == '2':
-            # Load label_dict kalau belum ada
             if label_dict is None:
                 label_dict = load_label_dict()
                 if label_dict is None:
                     print("Silakan lakukan registrasi terlebih dahulu untuk training otomatis.")
                     continue
-            recognize_face()  # langsung jalankan recognize yang upload ke Google Sheets
+            recognize_face() 
         elif choice == '3':
             print("Keluar program.")
             break
